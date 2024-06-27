0e62602d269fa9a308abd1f67039f5a713f0509d
Your Name <you@example.com>
2024-06-27 18:38:49 +0530

    react todo app basic

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import Todo from "./components/Todo";
+
+const App = () => {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text="Learn React" />
+      <Todo text="Master React" />
+      <Todo text="Explore the full React Course" />
+
+    </div>
+  );
+};
+
+export default App;
